8769c0abefa8e42bc193a1e6b93019f5908911ab
Emre Özbey <50449797+SabitEmreOzbey@users.noreply.github.com>
2020-12-15 23:47:56 +0300

    Initial commit

---
+++ update_product.php
@@ -0,0 +1,12 @@
+<?php
+// retrieve one product will be here
+ 
+// set page header
+$page_title = "Update Product";
+include_once "layout_header.php";
+ 
+// contents will be here
+ 
+// set page footer
+include_once "layout_footer.php";
+?>
